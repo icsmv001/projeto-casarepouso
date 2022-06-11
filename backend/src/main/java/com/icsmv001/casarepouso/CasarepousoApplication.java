6162a67e3c67c91f83be7349bdec68ae11e4fe85
icsmv001 <icsmv001@gmail.com>
2022-06-11 20:17:50 -0300

    criado projeto-casarepouso,v001, 20220611

---
+++ backend/src/main/java/com/icsmv001/casarepouso/CasarepousoApplication.java
@@ -0,0 +1,13 @@
+package com.icsmv001.casarepouso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasarepousoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasarepousoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/icsmv001/casarepouso/CasarepousoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icsmv001.casarepouso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasarepousoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
