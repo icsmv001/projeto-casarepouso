fb65c8560729a6b227adac2ba473a9c50e274484
icsmv001 <icsmv001@gmail.com>
2022-06-11 20:36:17 -0300

    revisando novo projeto-casarepouso,v001,20220611-sucesso.

--- backend/src/main/java/com/icsmv001/casarepouso/CasarepousoApplication.java
+++ backend/src/main/java/com/icsmv001/casarepouso/CasarepousoApplication.java
@@ -8,6 +8,7 @@ public class CasarepousoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CasarepousoApplication.class, args);
+		
 	}
 
 }
--- backend/src/test/java/com/icsmv001/casarepouso/CasarepousoApplicationTests.java
+++ backend/src/test/java/com/icsmv001/casarepouso/CasarepousoApplicationTests.java
@@ -8,6 +8,7 @@ class CasarepousoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
 	}
 
 }
