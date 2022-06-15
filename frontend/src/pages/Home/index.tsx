3fba7c980678e51a7ba4c2abbc1cf1f9e0bad9c1
icsmv001 <icsmv001@gmail.com>
2022-06-14 23:45:24 -0300

    Criado Navegancao e Estilizado paginas HOME e ListarPaciente

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,31 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Pagina Home</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4"> Casa de Repouso</h1>
+          <p className="lead">
+            Anbiente Agradavel, Amplo Espaço e Area Verde, Facil Acesso.
+          </p>
+          <hr />
+          <p>
+            A casa De Repouso, tem como missão garantir a estadia de seu ente
+            querido com o maior conforto e mantendo cuidadores 24hrs para
+            garantir sua integridade.
+          </p>
+
+          <Link className="btn btn-primary btn-lg" to="/ListarPaciente">
+            Lista de Pacientes
+          </Link>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 };
 
