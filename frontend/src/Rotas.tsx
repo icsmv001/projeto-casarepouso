3123b34b37d50d8f4ca75fbca6911ff1de12967a
icsmv001 <icsmv001@gmail.com>
2022-06-19 23:50:24 -0300

    atualizado react-router-dom versao 6

--- frontend/src/Rotas.tsx
+++ frontend/src/Rotas.tsx
@@ -1,5 +1,5 @@
-import React, { Route, Routes } from "react-router";
-import { BrowserRouter } from "react-router-dom";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "pages/Home";
 import ListarPaciente from "pages/ListarPaciente";
